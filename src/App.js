3c14808a31ee25980b9687425837464a1be1cbb9
hugoalves <halvesdepuga@gmail.com>
2023-11-01 11:45:55 +0100

    feat: navbar + new collection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 
 import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/guitares' element={<ShopCategory category="guitares"/>}/>
+        <Route path='/divers' element={<ShopCategory category="divers"/>}/>
+        <Route path='/apropos' element={<ShopCategory category="apropos"/>}/>
+        <Route path='/revendeur' element={<ShopCategory category="revendeur"/>}/>
+        <Route path='/contact' element={<ShopCategory category="contact"/>}/>
+        <Route path="product" element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
+import { Link } from 'react-router-dom'
 export const Navbar = () => {
+
+    const [menu,setMenu] = useState("accueil")
+
   return (
     <div className='navbar'>
         <div className='nav-logo'>
             <img src={logo} alt='' />
             <p>Alves de puga</p>
         </div>
-
+        <ul className='nav-menu'>
+            <li onClick={()=>{setMenu("accueil")}}><Link style={{ textDecoration: 'none'}} to='/'>Accueil</Link>{menu==="accueil"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("guitares")}}><Link style={{ textDecoration: 'none'}} to='/guitares'>Guitares</Link>{menu==="guitares"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("divers")}}><Link style={{ textDecoration: 'none'}} to='/divers'>Divers</Link>{menu==="divers"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("apropos")}}><Link style={{ textDecoration: 'none'}} to='/apropos'>A propos</Link>{menu==="apropos"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("revendeur")}}><Link style={{ textDecoration: 'none'}} to='/revendeur'>Revendeur</Link>{menu==="revendeur"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("contact")}}><Link style={{ textDecoration: 'none'}} to='/contact'>Contact</Link>{menu==="contact"?<hr/>:<></>}</li>
+        </ul>
     </div>
   )
 }
